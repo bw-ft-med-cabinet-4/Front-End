00f8e0dd97ec406621a40ec49a28e28381776521
Marissa <marissacooterl@gmail.com>
2020-04-30 19:15:00 -0500

    fixing CSS styles

--- med-cabinet/src/index.js
+++ med-cabinet/src/index.js
@@ -6,12 +6,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 // app
 import App from './App';
 // global style injections
-import Reset from './styles/Reset';
+// import Reset from './styles/Reset';
 
 
 ReactDOM.render(
   <Router>
-    <Reset />
+    {/* <Reset /> */}
     <App />
   </Router>,
   document.getElementById('root')
